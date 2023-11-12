7fb4db65cce04cec283dadab016afd5be7a3c3cb
Hessen Du <258414420@qq.com>
2023-11-12 05:54:38 +0000

    update

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -39,6 +39,22 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        // let team1_T = Team{goals_scored: team_1_score, goals_conceded: team_2_score};
+        // let team2_T = Team{goals_scored: team_2_score, goals_conceded: team_1_score};
+
+        let sc1 = scores.entry(team_1_name).or_insert(Team{goals_scored: 0, goals_conceded: 0});
+        // 这个函数的返回是一个ref，指向插入位置
+        (*sc1).goals_scored += team_1_score;
+        (*sc1).goals_conceded += team_2_score;
+
+        let sc2 = scores.entry(team_2_name).or_insert(Team{goals_scored: 0, goals_conceded: 0});
+        (*sc2).goals_scored += team_2_score;
+        (*sc2).goals_conceded += team_1_score;
+
+
+    
+
+        
     }
     scores
 }
